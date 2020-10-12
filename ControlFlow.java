8498316f194e04107b9a943dd1e2eef82eb18052
Cristian Garcia <cg7497@gmail.com>
2020-10-12 07:48:34 -0500

    Control Flow Hw

---
+++ ControlFlow.java
@@ -0,0 +1,21 @@
+/**
+ * ControlFlow
+ */
+public class ControlFlow {
+//The answer to the easy loop question is C. 20
+    public static void main(String[] args) {
+     
+        for (int i=0;i<=5;i++) {
+
+            for (int j=0;j<i;j++) {
+            System.out.print("* ");
+            }
+            System.out.println();
+            }
+            
+            
+        
+
+
+    }
+}
